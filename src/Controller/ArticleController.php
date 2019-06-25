7764c40ba66ce826bed742a604e785e14c5478b4
GyarmatiAdam <30920398+GyarmatiAdam@users.noreply.github.com>
2019-06-25 15:57:00 +0200

    installation
    twig and security checker installed

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,9 +1,10 @@
 <?php
 namespace App\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response as Something;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -17,6 +18,9 @@ class ArticleController
      */
     public function show($anything)
     {
-        return new Something(sprintf('here sholud be the url name: %s', $anything));
+       # return new Something(sprintf('here sholud be the url name: %s', $anything));
+       return $this->render('article/show.html.twig', [
+        'title' => ucwords(str_replace('-', ' ', $anything)),
+    ]);
     }
 }
