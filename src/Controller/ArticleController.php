8e866c00522a3aabe62439cc25f6ed0df697b766
GyarmatiAdam <30920398+GyarmatiAdam@users.noreply.github.com>
2019-06-25 17:13:36 +0200

    use twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -18,9 +18,15 @@ class ArticleController extends AbstractController
      */
     public function show($anything)
     {
+        $comments=['I ate a normal rock once. It did NOT taste like bacon!',
+        'Woohoo! I\'m going on an all-asteroid diet!',
+        'I like bacon too! Buy some from my site! bakinsomebacon.com',];
+
        # return new Something(sprintf('here sholud be the url name: %s', $anything));
+       
        return $this->render('article/show.html.twig', [
         'title' => ucwords(str_replace('-', ' ', $anything)),
+        'comments' => $comments,
     ]);
     }
 }
